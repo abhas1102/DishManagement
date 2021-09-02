a0014b87d18f070f3ac8d19ef0aa5a954555adcd
Abhas Kumar <abhas1102@gmail.com>
2021-09-03 03:41:15 +0530

    Added animation to the splash screen

--- app/src/main/java/com/example/goodfood/SplashActivity.kt
+++ app/src/main/java/com/example/goodfood/SplashActivity.kt
@@ -1,10 +1,15 @@
 package com.example.goodfood
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.example.goodfood.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -25,6 +30,26 @@ class SplashActivity : AppCompatActivity() {
 
             )
         }
+
+        val splashAnimation = AnimationUtils.loadAnimation(this,R.anim.anim_splash)
+        splashBinding.tvAppName.animation = splashAnimation
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+             //
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+               Handler(Looper.getMainLooper()).postDelayed({
+                   startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+                   finish()
+               },1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+               //
+            }
+
+        })
 
        // splashBinding.tvAppName.text = "Add Food"
     }
