85b1e657a5f949023947660586987a539bf41829
Abhas Kumar <abhas1102@gmail.com>
2021-09-03 13:22:41 +0530

    Implemented menu inflater to add & update dish

---
+++ app/src/main/java/com/example/goodfood/view/activities/activities/AddUpdateDishActivity.kt
@@ -0,0 +1,12 @@
+package com.example.goodfood.view.activities.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.goodfood.R
+
+class AddUpdateDishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update_dish)
+    }
+}
