cc1f6f9ca2ab294537912fc865f6af71a4c2cb24
Abhas Kumar <abhas1102@gmail.com>
2021-09-06 00:39:11 +0530

    Setup action bar and enabled back press

--- app/src/main/java/com/example/goodfood/view/activities/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/goodfood/view/activities/activities/AddUpdateDishActivity.kt
@@ -3,10 +3,24 @@ package com.example.goodfood.view.activities.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.goodfood.R
+import com.example.goodfood.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+    private lateinit var mBinding:ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        setUpActionBar()
+    }
+
+    private fun setUpActionBar(){
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
     }
 }
