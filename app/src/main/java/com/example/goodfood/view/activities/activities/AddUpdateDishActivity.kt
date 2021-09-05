4aa828051b3c22bc551ce963fc2ee957f794ee71
Abhas Kumar <abhas1102@gmail.com>
2021-09-06 01:32:59 +0530

    Showed the custom dialog when clicking on add image icon

--- app/src/main/java/com/example/goodfood/view/activities/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/goodfood/view/activities/activities/AddUpdateDishActivity.kt
@@ -1,11 +1,13 @@
 package com.example.goodfood.view.activities.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.goodfood.R
 import com.example.goodfood.databinding.ActivityAddUpdateDishBinding
+import com.example.goodfood.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity(),View.OnClickListener {
     private lateinit var mBinding: ActivityAddUpdateDishBinding
@@ -31,12 +33,32 @@ class AddUpdateDishActivity : AppCompatActivity(),View.OnClickListener {
         if (v != null) {
             when (v.id) {
                 R.id.iv_add_dish_image -> {
-                    Toast.makeText(this, "Clicked image", Toast.LENGTH_SHORT).show()
+                  customImageSelectionDialog()
                     return
                 }
             }
         }
 
+
+    }
+
+    private fun customImageSelectionDialog(){
+        val dialog = Dialog(this)
+        val binding:DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        binding.tvCamera.setOnClickListener {
+            Toast.makeText(this,"Camera clicked",Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+
+        }
+        binding.tvGallery.setOnClickListener {
+            Toast.makeText(this,"Gallery selected",Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+
+        }
+
+        dialog.show() // We use this to dialog to appear
 
     }
 }
