2f8c2eadc4370cd372c38d4fa95a2964b17c2005
Abhas Kumar <abhas1102@gmail.com>
2021-09-06 00:51:18 +0530

    Checked click listener action on icon image.

--- app/src/main/java/com/example/goodfood/view/activities/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/goodfood/view/activities/activities/AddUpdateDishActivity.kt
@@ -2,25 +2,41 @@ package com.example.goodfood.view.activities.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.goodfood.R
 import com.example.goodfood.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
-    private lateinit var mBinding:ActivityAddUpdateDishBinding
+class AddUpdateDishActivity : AppCompatActivity(),View.OnClickListener {
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
         setUpActionBar()
+
+        mBinding.ivAddDishImage.setOnClickListener(this)
     }
 
-    private fun setUpActionBar(){
+    private fun setUpActionBar() {
         setSupportActionBar(mBinding.toolbarAddDishActivity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
             onBackPressed()
         }
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_add_dish_image -> {
+                    Toast.makeText(this, "Clicked image", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+        }
+
 
     }
 }
