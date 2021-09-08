ee2c9dba5d70600715bf708c3c62905a731ff619
Abhas Kumar <abhas1102@gmail.com>
2021-09-09 04:15:22 +0530

    Completed setting up Room Database where DAO and entities will be accessed

---
+++ app/src/main/java/com/example/goodfood/model/database/GoodFoodRoomDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.goodfood.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.goodfood.model.entities.GoodFood
+
+
+@Database(entities = [GoodFood::class],version = 1,)
+abstract class GoodFoodRoomDatabase:RoomDatabase() {
+
+    companion object{
+        @Volatile
+        private var INSTANCE: GoodFoodRoomDatabase? = null
+
+        fun getDatabase(context: Context): GoodFoodRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    GoodFoodRoomDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
